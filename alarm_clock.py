0dd1e6e26725b09a80c2a26d4fdc8ebbb9c49ba5
Sameer Acharya <acharyasameer2002@gmail.com>
2023-11-07 22:25:26 -0500

    alarm clock in python

---
+++ alarm_clock.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        seconds_left = time_left % 60
+        minutes_left = time_left // 60
+        hours_left = minutes_left // 60
+
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in {hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
+
+    playsound("alarm.mp3")    
+
+hours = int(input("How many hours to wait: "))    
+minutes = int(input("How many minutes to wait: "))    
+seconds = int(input("How many seconds to wait: "))    
+total_seconds = hours * 60 * 60 + minutes * 60 + seconds
+
+alarm(total_seconds)
